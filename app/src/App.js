1d4fb0974caf5a1fd8af7bfc6ee1ebea7317c15e
Ej Matugas <franzelijahmatugas@gmail.com>
2022-01-20 20:18:46 +0800

    finish

--- app/src/App.js
+++ app/src/App.js
@@ -1,11 +1,11 @@
 import React, { useEffect, useState } from "react";
 import "./App.css";
 import CandyMachine from "./CandyMachine";
-import twitterLogo from "./assets/twitter-logo.svg";
+// import twitterLogo from "./assets/twitter-logo.svg";
 //2KkyUECUVPD4kteigFeEWVHCa9mz7rv6gYQCCJ4SQHw9
 // Constants
-const TWITTER_HANDLE = "_buildspace";
-const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
+// const TWITTER_HANDLE = "_buildspace";
+// const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
 const App = () => {
   const [walletAddress, setWalletAddress] = useState(null);
@@ -51,20 +51,11 @@ const App = () => {
     <div className="App">
       <div className="container">
         <div className="header-container">
-          <p className="header">🍭 Candy Drop</p>
-          <p className="sub-text">NFT drop machine with fair mint</p>
+          <p className="header">Spider Drop</p>
+          <p className="sub-text">Sum Spidermen Suit NFTs</p>
           {!walletAddress && renderNotConnectedContainer()}
         </div>
         {walletAddress && <CandyMachine walletAddress={window.solana} />}
-        <div className="footer-container">
-          <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
-          <a
-            className="footer-text"
-            href={TWITTER_LINK}
-            target="_blank"
-            rel="noreferrer"
-          >{`built on @${TWITTER_HANDLE}`}</a>
-        </div>
       </div>
     </div>
   );
