8254c6f7565e0dab0fe08ed3f6ff76ea72fd8e31
Ej Matugas <franzelijahmatugas@gmail.com>
2022-01-20 19:54:25 +0800

    all is good

--- app/src/App.js
+++ app/src/App.js
@@ -1,19 +1,61 @@
-import React from 'react';
-import './App.css';
-import twitterLogo from './assets/twitter-logo.svg';
-
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import CandyMachine from "./CandyMachine";
+import twitterLogo from "./assets/twitter-logo.svg";
+//2KkyUECUVPD4kteigFeEWVHCa9mz7rv6gYQCCJ4SQHw9
 // Constants
-const TWITTER_HANDLE = '_buildspace';
+const TWITTER_HANDLE = "_buildspace";
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
 const App = () => {
+  const [walletAddress, setWalletAddress] = useState(null);
+  const checkIfWalletIsConnected = async () => {
+    try {
+      const { solana } = window;
+      if (solana)
+        if (solana.isPhantom) {
+          console.log("Phantom Wallet Found");
+          const res = await solana.connect({ onlyIfTrusted: true });
+
+          setWalletAddress(res.publicKey.toString());
+        } else alert("solan object not found");
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  const connectWallet = async () => {
+    const { solana } = window;
+
+    if (solana) {
+      const res = await solana.connect();
+      setWalletAddress(res.publicKey.toString());
+    }
+  };
+
+  const renderNotConnectedContainer = () => (
+    <button
+      className="cta-button connect-wallet-button"
+      onClick={connectWallet}
+    >
+      Connect to Wallet
+    </button>
+  );
+  useEffect(() => {
+    const onLoad = async () => await checkIfWalletIsConnected();
+
+    window.addEventListener("load", onLoad);
+    return () => window.removeEventListener("load", onLoad);
+  }, []);
   return (
     <div className="App">
       <div className="container">
         <div className="header-container">
           <p className="header">🍭 Candy Drop</p>
           <p className="sub-text">NFT drop machine with fair mint</p>
+          {!walletAddress && renderNotConnectedContainer()}
         </div>
+        {walletAddress && <CandyMachine walletAddress={window.solana} />}
         <div className="footer-container">
           <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
           <a
